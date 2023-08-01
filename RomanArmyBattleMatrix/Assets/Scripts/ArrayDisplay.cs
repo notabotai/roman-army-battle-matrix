18864ddd18f8302baf62fd7382f4885f70c4d3d8
sid.under.attitude@gmail.com <116103188+SiddharthDarapuri@users.noreply.github.com>
2023-08-01 23:34:07 +0530

    Matrix

--- RomanArmyBattleMatrix/Assets/Scripts/ArrayDisplay.cs
+++ RomanArmyBattleMatrix/Assets/Scripts/ArrayDisplay.cs
@@ -1,29 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ArrayDisplay : MonoBehaviour
 {
+    NewBehaviourScript x;
+
     public int[,] myArray = new int[2, 2] { { 1, 2 }, { 3, 4 } };
 
-    public int[,] myArrayTwo = new int[2, 2] { { 2, 3 }, { 4, 5 } }; 
+    public int[,] myArrayTwo = new int[2, 2] { { 2, 3 }, { 4, 5 } };
+
+
+    public TextMeshPro One, Two, Three, Four;
+
+
     void Start()
     {
 
         for (int i = 0; i < myArray.GetLength(0); i++)
         {
-            for (int j = 0; j < myArray.GetLength(1)-(myArray.GetLength(1)/2); j++)
+            for (int j = 0; j < myArray.GetLength(1) - (myArray.GetLength(1) / 2); j++)
             {
                 int x = j;
 
-                Debug.Log(myArray[i, j]+" "+ myArray[i, x+1]);
+                Debug.Log(myArray[i, j] + " " + myArray[i, x + 1]);
             }
         }
+
+        myArray[0,0] = myArray[0,0] * myArrayTwo[0,0];
+        myArray[0, 1] = myArray[0, 1] * myArrayTwo[0, 1];
+        myArray[1, 0] = myArray[1, 0] * myArrayTwo[1, 0];
+        myArray[1, 1] = myArray[1, 1] * myArrayTwo[1, 1];
+
+        One.text = myArray[0, 0].ToString();
+        Two.text = myArray[0, 1].ToString();
+        Three.text = myArray[1, 0].ToString();
+        Four.text = myArray[1, 1].ToString();
+
+    }
 
         // Update is called once per frame
         void Update()
         {
 
         }
-    }
 }
---
+++ RomanArmyBattleMatrix/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    int r;
+
+    public GameObject a;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void XYZ()
+    {
+
+    }
+}
